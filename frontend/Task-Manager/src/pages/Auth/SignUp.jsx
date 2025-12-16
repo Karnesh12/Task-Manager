9fa585f8d750f7e2011a708fbbc7818dbbdc23b7
Karnesh <kaneshnirmal@gmail.com>
2025-12-16 14:55:00 +0530

    Login UI Form Done

--- frontend/Task-Manager/src/pages/Auth/SignUp.jsx
+++ frontend/Task-Manager/src/pages/Auth/SignUp.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import AuthLayout from "../../components/layouts/AuthLayout";
 
 const SignUp = () => {
+    const [profilePic, setProfilePic] = useState(null);
+    const [fullName, setFullName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [adminInviteToken, setAdminInviteToken] = useState('')
+
+    const [error, setError] = useState(null);
+    
     return (
-        <div>SignUp</div>
+        <AuthLayout>
+            h
+        </AuthLayout>
     )
 }
 
