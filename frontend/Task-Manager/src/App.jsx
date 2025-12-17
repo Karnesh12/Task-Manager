5b1d24dfb2d7411ba05dbdf9e6188e6887448661
Karnesh <kaneshnirmal@gmail.com>
2025-12-17 11:49:45 +0530

    Integrated Login API

--- frontend/Task-Manager/src/App.jsx
+++ frontend/Task-Manager/src/App.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
+  Outlet,
+  Navigate,
 } from "react-router-dom";
 import Login from "./pages/Auth/Login";
 import SignUp from "./pages/Auth/SignUp";
@@ -16,9 +18,11 @@ import MyTasks from "./pages/User/MyTasks";
 import ViewTaskDetails from "./pages/User/ViewTaskDetails";
 
 import PrivateRoute from "./routes/PrivateRoute";
+import UserProvider, { UserContext } from "./context/userContext";
 
 const App = () => {
   return (
+    <UserProvider>
     <div>
       <Router>
         <Routes>
@@ -40,10 +44,26 @@ const App = () => {
             <Route path="/user/task-details/:id" element={<ViewTaskDetails />} />
           </Route>
 
+          {/* Default Route */}
+          <Route path="/" element={<Root />} />
+
         </Routes>
       </Router>
     </div>
+    </UserProvider>
   )
 }
 
-export default App
+export default App;
+
+const Root = () => {
+  const { user, loading } = useContext(UserContext);
+
+  if(loading) return <Outlet />
+
+  if (!user) {
+    return <Navigate to="/login" />;
+  }
+
+  return user.role === "admin" ? <Navigate to="/admin/dashboard" /> : <Navigate to="/user/dashboard" />;
+};
