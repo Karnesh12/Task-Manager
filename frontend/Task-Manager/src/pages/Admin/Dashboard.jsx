0561e38f9bfdced2a291e7f7123ba5ce7632d7ab
Karnesh <kaneshnirmal@gmail.com>
2026-01-15 13:29:40 +0530

    fix the greeting part

--- frontend/Task-Manager/src/pages/Admin/Dashboard.jsx
+++ frontend/Task-Manager/src/pages/Admin/Dashboard.jsx
@@ -66,6 +66,15 @@ const Dashboard = () => {
         navigate('/admin/tasks')
     }
 
+    const getGreeting = () => {
+        const hour = new Date().getHours();
+
+        if (hour >= 5 && hour < 12) return "Good Morning";
+        if (hour >= 12 && hour < 17) return "Good Afternoon";
+        if (hour >= 17 && hour < 21) return "Good Evening";
+        return "Good Night";
+    };
+
     useEffect(() => {
         getDashboardData();
 
@@ -76,7 +85,7 @@ const Dashboard = () => {
         <div className="card my-5">
             <div>
                 <div className="col-span-3">
-                    <h2 className="text-xl md:text-2xl">Good Morning! {user?.name}</h2>
+                    <h2 className="text-xl md:text-2xl">{getGreeting()}, {user?.name}</h2>
                     <p className="text-xs md:text-[13px] text-gray-400 mt-1.5">
                         {moment().format("dddd Do MMM YYYY")}
                     </p>
